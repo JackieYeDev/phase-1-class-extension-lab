4699ee91582f12007ec81c7ea3022700191dbc31
Jackie <jye0325@gmail.com>
2022-05-24 12:03:35 -0400

    commit tree 03e8f571dc1f22f6246a73770402f9e5fecef45d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+    get countSides() {
+        return this.sides.length;
+    };
+    get perimeter() {
+        return this.sides.reduce((a,b) => a+b);
+    };
+};
+
+class Triangle extends Polygon {
+    get isValid() {
+        if(this.countSides !== 3) return false;
+        if(Math.max(...this.sides) > (this.perimeter - Math.max(...this.sides))) return false;
+        return true;
+    }
+};
+
+class Square extends Polygon {
+    get isValid() {
+        if(this.countSides !== 4) return false;
+        if(this.sides.filter((e) =>  e !== this.perimeter/4).length != 0) return false;
+        return true;
+    };
+
+    get area() {
+        return Math.pow(this.sides[0], 2);
+    };
+}
